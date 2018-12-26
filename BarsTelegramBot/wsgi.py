58fa57b799c38fcf1c2ae6aa631b1b38edb58a12
Rustem Yamikov <jarustem@gmail.com>
2018-12-26 14:25:56 +0300

    fix

--- BarsTelegramBot/wsgi.py
+++ BarsTelegramBot/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sampleproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BarsTelegramBot.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
