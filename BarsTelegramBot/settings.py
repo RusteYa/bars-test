0b3ba126368b2f4f38154c1cb45ecb5bed35b177
Rustem Yamikov <jarustem@gmail.com>
2018-12-26 13:28:09 +0300

    fix

--- BarsTelegramBot/settings.py
+++ BarsTelegramBot/settings.py
@@ -189,7 +189,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(_PATH, '../bot/static'),
+    os.path.join(PROJECT_ROOT, '../BarsTelegramBot/bot/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
